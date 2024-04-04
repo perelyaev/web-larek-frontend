472872524a2cf813a2aa5df4a1fb9fbe9721f927
perelyaev <perelyaev@gmail.com>
2024-04-04 21:12:05 +0800

    На ревью 04.04.2024

--- src/components/base/api.ts
+++ src/components/base/api.ts
@@ -2,7 +2,6 @@ export type ApiListResponse<Type> = {
     total: number,
     items: Type[]
 };
-
 export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
 
 export class Api {
--- src/components/base/events.ts
+++ src/components/base/events.ts
@@ -18,6 +18,7 @@ export interface IEvents {
  * В расширенных вариантах есть возможность подписаться на все события
  * или слушать события по шаблону например
  */
+
 export class EventEmitter implements IEvents {
     _events: Map<EventName, Set<Subscriber>>;
 
